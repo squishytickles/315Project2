a1c8323341cd756db951b9d5585d20000d0eb796
alecwoelffer <alec_woelffer_1@neo.tamu.edu>
2013-03-30 12:43:00 -0500

    Update MinimaxTree.java

--- MinimaxTree.java
+++ MinimaxTree.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionListener;
 import java.util.*;
 
 public class MinimaxTree {
-  
+	
 	private Node root; 
 
 	//Constructor 
@@ -25,22 +25,27 @@ public class MinimaxTree {
 	public void setRoot(Node rootElement){
 		this.root = rootElement; 
 	}
-	
-	public void BuildMiniMaxTree(Node rt, int depth){
-		root = rt; 
-		Node child; 
+	//*****need to finish implementation 
+	public void BuildMiniMaxTree(Node n, int depth){ 
 		if(depth >= 0){
-			for(int = 0; i < /*size of move list*/; ++i){
-				//place all move values within the tree 
-				child = new Node(/*move list value*/, depth-1);
-				children.add(child);
+			n = new Node();
+			root = n; 
+			//place all move values within the tree 
+			for(int i = 0; i < validMoves.size(); ++i){
+				n.setNodePiece(validMoves.get(i).destRow, validMoves.get(i).destColumn, validMoves.get(i).direction, validMoves.get(i).color);
+				children.add(n);
+				//needs work 
+				BuildMiniMaxTree(n.children.get(i),depth-1);
+				
 			}
 		}
+		else
+			n = new Node();  
 	}
 	//MiniMax Algorithm 
-	public void getBestMove(){
+	public movePiece getBestMove(){
 
-		ArarayList<Node> root_children = root.getChildren();
+		ArrayList<Node> root_children = root.getChildren();
 
 	
 		int index_max = 0; 
@@ -51,8 +56,9 @@ public class MinimaxTree {
 
 		if(root.isLeaf()){
 			//call evaluate function 
+			evalBoard(); 
 		}
-		for(int i = 0; i < root_children.size(); ++i){
+		for(int i = 1; i < root_children.size(); ++i){
 
 			if(value > max){
 				max = value; 
@@ -66,11 +72,13 @@ public class MinimaxTree {
 
 		if(root.isMax()){
 			//return move
-			//return root_children.get(index_max)
+			pieceMove bestMove = new pieceMove(root_children.get(index_max).x, root_children.get(index_max).y, root_children.get(index_max).direction, root_children.get(index_max).color);
+			return movePiece(bestMove.destRow, bestMove.destColumn); 
 		}
 		if(root.isMin()){
 			//return move 
-			//return root_children.get(index_min)
+			pieceMove bestMove = new pieceMove(root_children.get(index_min).x, root_children.get(index_min).y, root_children.get(index_min).direction, root_children.get(index_min).color);
+			return movePiece(bestMove.destRow, bestMove.destColumn); 
 		}
 
 
@@ -83,6 +91,13 @@ public class Node{
 	private int value; 
 	private int depth; 
 	public ArrayList<Node> children;
+
+	//Holds each piecemove values 
+	private int x;
+	private int y;
+	private String direction; 
+	private int color; //color in pieceMove is private, need to change 
+	
 	
 
 	//Constructor 
@@ -144,5 +159,11 @@ public class Node{
 		} 
 		else 
 			return false; 
+	}
+	public void setNodePiece(int _x, int _y, String _s, int _c){
+		x = _x;
+		y = _y;
+		direction = _s; 
+		color = _c; 
 	}
 }
